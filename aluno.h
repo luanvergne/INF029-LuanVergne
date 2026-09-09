794a21c822302a16b34091cb7f7d82bee1212e1f
luanvergne <luanvergne10@gmail.com>
2026-09-09 16:42:59 -0300

    Modularização aluno.h

---
+++ aluno.h
@@ -0,0 +1,27 @@
+```c
+#ifndef ALUNO_H
+#define ALUNO_H
+
+#define TAM_ALUNO 3
+
+typedef struct alu {
+    int matricula;
+    char sexo;
+    int ativo;
+} Aluno;
+
+
+/* Funções do módulo Aluno */
+
+int cadastrarAluno(Aluno lista_aluno[], int qtdAluno);
+
+void listarAlunos(Aluno lista_aluno[], int qtdAluno);
+
+void atualizarAluno(Aluno lista_aluno[], int qtdAluno);
+
+int excluirAluno(Aluno lista_aluno[], int qtdAluno);
+
+int menuAluno(Aluno lista_aluno[], int qtdAluno);
+
+#endif
+```
