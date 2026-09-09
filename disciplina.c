aeb4ca94216cb9304f42b350131b32b8d512d34a
luanvergne <luanvergne10@gmail.com>
2026-09-09 16:46:16 -0300

    Modularização disciplina.c

---
+++ disciplina.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "disciplina.h"
+
+
+void menuDisciplina(void) {
+
+    printf("======== Módulo Disciplina ========\n");
+
+    printf("Módulo ainda em desenvolvimento.\n");
+}
