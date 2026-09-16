5c14bc2de41c65bd96b49c86ea26d81384b6c379
luanvergne <luanvergne10@gmail.com>
2026-09-16 16:17:43 -0300

    disciplina.h

--- ProjetoEscola/disciplina.h
+++ ProjetoEscola/disciplina.h
@@ -1,6 +1,34 @@
 #ifndef DISCIPLINA_H
 #define DISCIPLINA_H
 
-void menuDisciplina(void);
+#define TAM_DISCIPLINA 30
+#define TAM_NOME_DISCIPLINA 100
+#define TAM_ALUNOS_DISCIPLINA 40
+
+typedef struct {
+    char nome[TAM_NOME_DISCIPLINA];
+    int codigo;
+    int semestre;
+    int matriculaProfessor;
+
+    int alunos[TAM_ALUNOS_DISCIPLINA];
+    int qtdAlunos;
+
+    int ativo;
+
+} Disciplina;
+
+Disciplina cadastrarDisciplina();
+Disciplina atualizarDisciplina(Disciplina disciplina);
+
+int inserirAlunoDisciplina
+(Disciplina disciplina, int matriculaAluno);
+
+int removerAlunoDisciplina
+(Disciplina disciplina,int matriculaAluno);
+
+int menuDisciplina(Disciplina listaDisciplina[],int qtdDisciplina);
+
+void listarDisciplina(Disciplina listaDisciplina[], int qtdDisciplina);
 
 #endif
