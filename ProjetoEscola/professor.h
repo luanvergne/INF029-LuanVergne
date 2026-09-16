e67210b2265b77a929c6da8ab832c5d611d4fca5
luanvergne <luanvergne10@gmail.com>
2026-09-16 15:59:18 -0300

    professor.h

--- ProjetoEscola/professor.h
+++ ProjetoEscola/professor.h
@@ -1,7 +1,24 @@
 #ifndef PROFESSOR_H
-
 #define PROFESSOR_H
 
-void menuProfessor(void);
+#include "pessoa.h"
+
+#define TAM_PROFESSOR 50
+
+typedef struct {
+    int matricula;
+    Pessoa pessoa;
+    int ativo;
+} Professor;
+
+Professor cadastrarProfessor();
+Professor atualizarProfessor (Professor professor);
+
+int excluirProfessor(Professor listaProfessor[], int qtdProfessor);
+
+int menuProfessor(Professor listaProfessor[], int qtdProfessor);
+
+void listarProfessor(Professor listaProfessor[], int qtdProfessor);
 
 #endif
+```
